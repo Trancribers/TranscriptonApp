cf705aad1cfccc31221bc087f84c601918b84e2c
Trancribers <leggentxt@gmail.com>
2024-09-04 01:51:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,20 @@ import account, application
 import os
 
 
-class multiApp:
 
-    def __init__(self):
-       self.useremail=None
-       self.username= None
+def run():
+    st.header("Welcome to Transcribers")
+    with st.sidebar:
+        app=option_menu(
+        menu_title='Transcribers',
+        options=['Account','Transcribe'],
+        icons=['person-circle','trophy-fill'],
+        menu_icon='chat-text-fill',
+        default_index=1
+        )
 
-
-    def run():
-        st.header("Welcome to Transcribers")
-        with st.sidebar:
-            app=option_menu(
-                menu_title='Transcribers',
-                options=['Account','Transcribe'],
-                icons=['person-circle','trophy-fill'],
-                menu_icon='chat-text-fill',
-                default_index=1
-            )
-
-        if app =='Account': 
-            account.app()
-        if app=='Transcribe': 
-            application.app()
-
-    run()
+    if app =='Account': 
+        account.app()
+    if app=='Transcribe': 
+        application.app()
+run()

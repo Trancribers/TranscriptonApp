aa3b64ecbf96b178b53fa196f88f10064ac2147e
Trancribers <leggentxt@gmail.com>
2024-09-04 01:45:15 +0530

    Update account.py

--- account.py
+++ account.py
@@ -25,48 +25,41 @@ try:
     firebase_admin.get_app()
 except ValueError as e:
     initialize_app(cred)
+
+google_cred={"web":{
+    "client_id":st.secrets["google_oauth"]["client_id"],
+    "project_id":st.secrets["google_oauth"]["project_id"],
+    "auth_uri":st.secrets["google_oauth"]["auth_uri"],
+    "token_uri":st.secrets["google_oauth"]["token_uri"],
+    "auth_provider_x509_cert_url":st.secrets["google_oauth"]["auth_provider_x509_cert_url"],
+    "client_secret":st.secrets["google_oauth"]["client_secret"],
+    "redirect_uris":st.secrets["google_oauth"]["redirect_uris"]
+}}
+with tempfile.NamedTemporaryFile(delete=False, suffix=".json",mode='w') as temp_file:
+    # Write JSON content to the temporary file
+    json.dump(google_cred, temp_file)
+    temp_file.flush()
+    # Streamlit Authentication
+authenticator = Authenticate(
+    secret_credentials_path = temp_file.name,
+    cookie_name='nixon_cookie_name',
+    cookie_key='nixon_secret',
+    redirect_uri="https://transcribers.streamlit.app/",
+    )
+
 def app():
-    
-    google_cred={"web":{
-        "client_id":st.secrets["google_oauth"]["client_id"],
-        "project_id":st.secrets["google_oauth"]["project_id"],
-        "auth_uri":st.secrets["google_oauth"]["auth_uri"],
-        "token_uri":st.secrets["google_oauth"]["token_uri"],
-        "auth_provider_x509_cert_url":st.secrets["google_oauth"]["auth_provider_x509_cert_url"],
-        "client_secret":st.secrets["google_oauth"]["client_secret"],
-        "redirect_uris":st.secrets["google_oauth"]["redirect_uris"]
-    }}
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".json",mode='w') as temp_file:
-        # Write JSON content to the temporary file
-        json.dump(google_cred, temp_file)
-        temp_file.flush()
-        # Streamlit Authentication
-    authenticator = Authenticate(
-        secret_credentials_path = temp_file.name,
-        cookie_name='nixon_cookie_name',
-        cookie_key='nixon_secret',
-        redirect_uri="https://transcribers.streamlit.app/",
-        )
-
-
     # Check if the user is authenticated
     authenticator.check_authentification()
     
     st.title('Account')    
     
+    authenticator.login()
+    
     if st.session_state['connected']:
         st.image(st.session_state['user_info'].get('picture'))
-        st.write('Hello, ' + st.session_state['user_info'].get('name'))
-        st.write('Your email is ' + st.session_state['user_info'].get('email'))
+        st.write('Hello, '+ st.session_state['user_info'].get('name'))
+        st.write('Your email is '+ st.session_state['user_info'].get('email'))
         if st.button('Log out'):
             authenticator.logout()
-            st.session_state['connected'] = False
-            st.session_state['user_info'] = {}
-            st.write("You have been logged out.")
-    else:
-        st.write('You are not connected')
-        authorization_url = authenticator.get_authorization_url()
-        st.markdown(f'[Login]({authorization_url})')
-        st.link_button('Login', authorization_url)
         
 app()

a0ba1244001430116858797312ff991a2350447a
Trancribers <leggentxt@gmail.com>
2024-09-04 02:28:50 +0530

    Update account.py

--- account.py
+++ account.py
@@ -29,7 +29,7 @@ except ValueError as e:
 client_id=st.secrets["google_oauth"]["client_id"]
 project_id=st.secrets["google_oauth"]["project_id"]
 auth_uri=st.secrets["google_oauth"]["auth_uri"]
-token_uri=:st.secrets["google_oauth"]["token_uri"]
+token_uri=st.secrets["google_oauth"]["token_uri"]
 auth_provider_x509_cert_url=st.secrets["google_oauth"]["auth_provider_x509_cert_url"]
 client_secret=st.secrets["google_oauth"]["client_secret"]
 redirect_uris=st.secrets["google_oauth"]["redirect_uris"]
